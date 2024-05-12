8030e5c0c9cdbdf96b02c548f7b99cf31e61c932
2000krysztof <2000krzysztof@gmail.com>
2024-05-12 15:06:23 +0100

    Create PixelGraphClass

---
+++ Assignment2/src/main/java/com/example/assignment2/utils/PixelGraph.java
@@ -0,0 +1,33 @@
+package com.example.assignment2.utils;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+
+public class PixelGraph {
+
+
+    public static int[] breadthFirstSearch(int origin, int destination,  int[] pixels){
+
+
+        return null;
+    }
+
+   static int[] getPixels(Image image){
+       PixelReader reader = image.getPixelReader();
+       int width = (int)image.getWidth();
+       int size = (int)(width*image.getHeight());
+       int[] result = new int[size];
+       for(int i=0; i< size;i++){
+           int x = i%width;
+           int y = i/width;
+           int color = reader.getArgb(x,y);
+           if(color != 0xff000000){
+              result[i] = 1;
+           }else {
+               result[i] = 0;
+           }
+       }
+       return result;
+   }
+
+}
