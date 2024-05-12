6671db3cf7b3f026cb3b147b9d549e2978af02f8
Paul123111 <20102629wit@gmail.com>
2024-05-12 15:14:54 +0100

    add PixelGraphTest

---
+++ Assignment2/src/main/test/com/example/assignment2/utils/PixelGraphTest.java
@@ -0,0 +1,21 @@
+package com.example.assignment2.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PixelGraphTest {
+
+    @org.junit.jupiter.api.Test
+    void breadthFirstSearch() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPixels() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void asciiImage() {
+
+    }
+}
